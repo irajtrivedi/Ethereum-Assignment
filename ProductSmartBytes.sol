a2df53b1e81881fe5556aec98c1a318882e8e3c4
Raj Trivedi <32588108+irajtrivedi@users.noreply.github.com>
2019-08-28 16:10:44 +0530

    Add files via upload

--- ProductSmartBytes.sol
+++ ProductSmartBytes.sol
@@ -13,36 +13,37 @@ contract ProductSmartBytes {
         bytes32 Color;
         uint Status;
     }
-    
+
     event ProductCreated(
-        bytes32 Id,
-        bytes32 Name,
-        bytes32 Color,
-        uint Status 
+        string Id,
+        string Name,
+        string Color
     );
-    
+
     event OwnerChanged(
         string Id,
-        address NewOwner 
+        address NewOwner
     );
-    
+
     mapping(uint => Product) products; //Mapping key to Product
     string[] productidList;
-    
-    
-    function addProduct(string memory _id, string memory _name, string memory  _color) payable public {
+
+
+    function addProduct(string memory _id, string memory _name, string memory  _color) public payable {
         products[prodCount] = Product(msg.sender, convertToBytes32(_id), convertToBytes32(_name), convertToBytes32(_color), 0);
         productidList.push(_id);
+        // Event returns values in String so that same can be captured in GUI. We need not have to convert in Web3.js
+        emit ProductCreated(_id, _name, _color);
         prodCount += 1;
     }
-    
-    function changeProductOwner(string memory _productid, address _newOwner) payable public checkOwner(_productid){
+
+    function changeProductOwner(string memory _productid, address _newOwner) public payable checkOwner(_productid){
         products[getIndex(_productid)].Owner = _newOwner;
         emit OwnerChanged(_productid, _newOwner);
     }
-    
-    function getProductById(string memory _id) public returns (Product memory){
-        for(uint i=0;i<productidList.length;i++){
+
+    function getProductById(string memory _id) public view returns (Product memory){
+        for(uint i = 0 ; i<productidList.length ; i++){
             if(products[i].Id == convertToBytes32(_id)){
                 Product memory _dummy;
                 _dummy.Owner = products[i].Owner;
@@ -50,23 +51,22 @@ contract ProductSmartBytes {
                 _dummy.Name = products[i].Name;
                 _dummy.Color = products[i].Color;
                 _dummy.Status = products[i].Status;
-                emit ProductCreated(products[i].Id, products[i].Name, products[i].Color, products[i].Status);
-                //return products[i];
+                return _dummy;
             }
         }
     }
-    
+
     function getProductsCount() public view returns (uint productCount){
         return productidList.length;
     }
-    
-    function getProductByPos(uint _ind)view public returns (Product memory){
+
+    function getProductByPos(uint _ind)public view returns (Product memory){
         return products[_ind];
     }
-    
+
     // Helper Functions and Modifiers defined below
-    
-    function getIndex(string memory _productid) private returns(uint){
+
+    function getIndex(string memory _productid) private view returns(uint){
         uint i;
         for (i = 0; i<productidList.length; i++){
             if(products[i].Id == convertToBytes32(_productid)){
@@ -74,8 +74,8 @@ contract ProductSmartBytes {
             }
         }
     }
-    
-    function convertToBytes32(string memory source) view private returns (bytes32 result) {
+
+    function convertToBytes32(string memory source) private pure returns (bytes32 result) {
         bytes memory tempEmptyStringTest = bytes(source);
         if (tempEmptyStringTest.length == 0) {
             return 0x0;
@@ -84,16 +84,28 @@ contract ProductSmartBytes {
             result := mload(add(source, 32))
         }
     }
-    
+
     modifier checkOwner(string memory _productid){
-        require(products[getIndex(_productid)].Owner == msg.sender);
+        require(products[getIndex(_productid)].Owner == msg.sender, "Only owners can change the Owner of a Product!");
         _;
     }
-    
-    //function convertToString(bytes32 memory _byt)public returns(string memory){
-    //    string memory converted = string(_byt);
-    //    return converted;
-    //}
+
+    function bytes32ToString(bytes32 x) private pure returns (string memory) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (uint j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
+    }
 }
 
 // Dummy Products
